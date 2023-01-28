784371e1819d25cc65dfa3508045920a181b34fb
Umer-Aziz <umars25271997@gmail.com>
2023-01-28 13:40:44 +0500

    workflow card updated

--- components/workflow/MainContent.js
+++ components/workflow/MainContent.js
@@ -10,19 +10,39 @@ const MainContent = () => {
             <div className={style.cardWrapper}>
                 <Cardheader title="Backlog" counter="2"/>
                 <div className={style.Workflowcard}>
-                <Workflowcard/>
-                <Workflowcard/>
+
+                {/* add class name of colors in dotColor & image name in Icon.. / images
+                      all Icons must be in svg format  */}
+
+                {/* CARD 1 */}
+                <Workflowcard dotColor="pinkDot" cardLabel="ContactRm" cardHeadIcon="userstar" cardTitle="Design a website" 
+                cardTagTitle1="api.contactrm.com" cardTagTitle2="api.contactrm"  CreatedTime="Created 3 days ago" userProfilePic="user2.png"/>
+
+                 {/* CARD 2  */}
+                <Workflowcard dotColor="yellowDot" cardLabel="AgentBook" cardHeadIcon="filecheck" cardTitle="Design a website" 
+                cardTagTitle1="api.contactrm.com" cardTagTitle2="api.contactrm" IconChecklist2="checklist2" ChecklistCounter="8/9"
+                    CreatedTime="Created 3 days ago" userProfilePic="user2.png"
+                />
                 </div>
+
             </div>
+
+            {/* TODO CARDS  CONTAINER */}
             <div className={style.cardWrapper}>
                 <Cardheader title="To Do" counter="5"/>
             </div>
+
+            {/* IN PROCESS CARDS CONTAINER */}
             <div className={style.cardWrapper}>
                 <Cardheader title="In Process" counter="35"/>
             </div>
+
+            {/* IN REVIEW CARDS CONTAINER */}
             <div className={style.cardWrapper}>
                 <Cardheader title="In Review" counter="5"/>
             </div>
+
+           {/* COMPLETED CARDS CONTAINER */}
             <div className={style.cardWrapper}>
                 <Cardheader title="Completed" counter="1"/>
             </div>
--- components/workflow/Workflowcard.js
+++ components/workflow/Workflowcard.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import style from "@/styles/workflow/workflowcard.module.css"
-const Workflowcard = () => {
+const Workflowcard = ({ dotColor , cardLabel , cardHeadIcon , cardTitle , cardTagTitle1 , cardTagTitle2 ,
+     IconMessage , IconChecklist2 , IconLink , MessageCounter , ChecklistCounter , LinkCounter , CreatedTime , userProfilePic }) => {
   return (
     <>
             <div className={style.cardWrapper}>
                {/* card Inner head */}
                 <div className={style.cardInnerHeadWrapper}>
                 <div className={style.cardInnerHead}>
-                    <div className={style.cardDot}></div>
-                    <span className={style.cardLabel}>ContactRm</span>
+                    <div className={`${style.cardDot} ${dotColor}`}></div>
+                    <span className={style.cardLabel}>{cardLabel}</span>
                 </div>
                 <div>
-                    <img className={style.cardHeadIcon} src="/icons/filecheck.svg" alt="file icon" />
+                    <img className={style.cardHeadIcon} src={`/icons/${cardHeadIcon}.svg`} alt="icon" />
                 </div>
                 </div>
                 {/* card body  */}
                 <div style={style.cardBodyWrapper}>
-                    <p className={style.cardTitle}>Design a website</p>
+                    <p className={style.cardTitle}>{cardTitle}</p>
                     <div className={style.cardTagsWrapper}>
-                        <span className={`${style.cardTagTitle} ${style.cardBgYellow}`}>api.contactrm.com</span>
-                        <span className={`${style.cardTagTitle} ${style.cardBgPink}`}>api.contactrm</span>
+                        <span className={`${style.cardTagTitle} ${style.cardBgYellow}`}>{cardTagTitle1}</span>
+                        <span className={`${style.cardTagTitle} ${style.cardBgPink}`}>{cardTagTitle2}</span>
                     </div>
 
                     {/* card Icons  */}
@@ -28,27 +29,32 @@ const Workflowcard = () => {
                     <li className={style.cardIconItem}>
                         <img className={style.cardIcon} src="/icons/description.svg" alt="icon" />
                     </li>
-                    <li className={style.cardIconItem}>
-                        <img className={style.cardIcon} src="/icons/message.svg" alt="icon" />
-                        <span className={style.IconCounterText}>12</span>
-                    </li>    
-                    <li className={style.cardIconItem}>
-                        <img className={style.cardIcon} src="/icons/checklist2.svg" alt="icon" />
-                        <span className={style.IconCounterText}>8/9</span>
-                    </li>
-                    <li className={style.cardIconItem}>
-                        <img className={style.cardIcon} src="/icons/link.svg" alt="icon" />
-                        <span className={style.IconCounterText}>2</span>
-                    </li>
+
+                    { IconMessage != undefined && <li className={style.cardIconItem}>
+                        <img className={style.cardIcon} src={`/icons/${IconMessage}.svg`} alt="icon" />
+                        <span className={style.IconCounterText}>{MessageCounter}</span>
+                    </li> } 
+
+                    { IconChecklist2 != undefined && <li className={style.cardIconItem}>
+                        <img className={style.cardIcon} src={`/icons/${IconChecklist2}.svg`} alt="icon" />
+                        <span className={style.IconCounterText}>{ChecklistCounter}</span>
+                    </li>}
+
+                   
+                    { IconLink != undefined && <li className={style.cardIconItem}>
+                        <img className={style.cardIcon} src={`/icons/${IconLink}.svg`} alt="icon" />
+                        <span className={style.IconCounterText}>{LinkCounter}</span>
+                    </li>}
+
                     </ul>
                     </div>
 
                     {/* card bottom */}
                     <div className={style.BottomWrapper}>
                     <div className={style.textWrapper}>
-                        <p className={style.bottomText}>Created 3 days ago</p>
+                        <p className={style.bottomText}>{CreatedTime}</p>
                     </div>
-                        <img className={style.bottomIcon} src="/images/user2.png" alt="user pic" />
+                        <img className={style.bottomIcon} src={`/images/${userProfilePic}`} alt="user pic" />
                     </div>
                 </div>
         </div>
